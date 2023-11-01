bf5f59a52a3cbc6296605a6ab42c04e2c28061ca
DmitriyTR <skate1233@mail.ru>
2023-11-01 21:45:17 +0300

    Добавьте файлы проекта.

---
+++ Homework 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер матрицы (n): ");
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0)
+        {
+            int[,] matrix = new int[n, n];
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Введите элемент [{i},{j}]: ");
+                    if (int.TryParse(Console.ReadLine(), out int element))
+                    {
+                        matrix[i, j] = element;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+                        j--;
+                    }
+                }
+            }
+
+            int diagonalSum = 0;
+            int belowDiagonalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                diagonalSum += matrix[i, i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    belowDiagonalSum += matrix[i, j];
+                }
+            }
+
+            Console.WriteLine($"Сумма элементов побочной диагонали: {diagonalSum}");
+            Console.WriteLine($"Сумма элементов под побочной диагональю: {belowDiagonalSum}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный размер матрицы. Введите положительное число.");
+        }
+    }
+}
